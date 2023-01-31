0dd3aac70cbe7b27743c57e95c3b9a6837258faf
Theo Mironidis <mironidis.gr@gmail.com>
2023-01-31 17:25:48 +0000

    add button

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Alo monsieur</Text>
+      <Button
+        onPress={() => console.log('Alo')}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
       <StatusBar style="auto" />
     </View>
   );
